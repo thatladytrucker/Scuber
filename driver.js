938179043d23bc368ae263383c32b08f3d7c296e
thatladytrucker <thatladytrucker@gmail.com>
2026-07-10 17:32:49 -0400

    Add driver data with availability and ratings
    This is our temporary driver list.

---
+++ driver.js
@@ -0,0 +1,24 @@
+const drivers = [
+
+    {
+        name: "Alex",
+        available: true,
+        area: "Rural Area",
+        rating: 4.9
+    },
+
+    {
+        name: "Jordan",
+        available: true,
+        area: "City Area",
+        rating: 4.8
+    },
+
+    {
+        name: "Taylor",
+        available: false,
+        area: "Rural Area",
+        rating: 5.0
+    }
+
+];
