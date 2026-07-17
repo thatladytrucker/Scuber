224faec0201894d1c6ddba178fdff4ea45fd874c
thatladytrucker <thatladytrucker@gmail.com>
2026-07-17 16:10:30 -0400

    Refactor main app visibility handling
    Refactor code to store main app element in a variable before removing hidden class and log its class names.

--- app.js
+++ app.js
@@ -185,8 +185,11 @@ function showRiderHome() {
     document.getElementById("rider-trip-screen")
     .classList.add("hidden");
 
-    document.getElementById("main-app")
-    .classList.remove("hidden");
+    let home = document.getElementById("main-app");
+
+home.classList.remove("hidden");
+
+console.log("Main app classes:", home.className);
 
 }
 // ===========================================
