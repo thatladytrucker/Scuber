bf50a848ae5fcac67f9e44c80d6af63819cea340
thatladytrucker <thatladytrucker@gmail.com>
2026-07-31 19:37:57 -0400

    Initialize Firebase with configuration
    Added Firebase initialization and configuration.

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,9 @@
 // SCUBER Firebase Configuration
 
+import { initializeApp } from 
+"https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+
+
 const firebaseConfig = {
     apiKey: "YOUR_API_KEY",
     authDomain: "YOUR_AUTH_DOMAIN",
@@ -8,3 +12,10 @@ const firebaseConfig = {
     messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
     appId: "YOUR_APP_ID"
 };
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+console.log("SCUBER Firebase Connected:", app.name);
