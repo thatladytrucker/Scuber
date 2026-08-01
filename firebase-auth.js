7cf25b585343d924e15255a9e84ac10eaaa0a6b2
thatladytrucker <thatladytrucker@gmail.com>
2026-08-01 15:07:41 -0400

    First Authentication Function

--- firebase-auth.js
+++ firebase-auth.js
@@ -1,1 +1,39 @@
+import {
+  getAuth,
+  createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
 
+const auth = getAuth();
+
+async function createFirebaseUser(email, password) {
+
+    try {
+
+        const userCredential =
+            await createUserWithEmailAndPassword(
+                auth,
+                email,
+                password
+            );
+
+        console.log(
+            "Firebase User Created:",
+            userCredential.user.uid
+        );
+
+        return userCredential.user;
+
+    } catch (error) {
+
+        console.error(
+            "Firebase Error:",
+            error.code,
+            error.message
+        );
+
+        alert(error.message);
+
+        return null;
+    }
+
+}
