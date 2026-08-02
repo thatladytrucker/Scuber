bb0442dadd935b91b47ecc71ea8896d3ae46ecb3
thatladytrucker <thatladytrucker@gmail.com>
2026-08-02 16:23:41 -0400

    Expose deleteCurrentUser function to the window object

--- app.js
+++ app.js
@@ -1054,3 +1054,4 @@ window.goOnline = goOnline;
 window.goOffline = goOffline;
 window.showDriverEarnings = showDriverEarnings;
 window.selectUser = selectUser;
+window.deleteCurrentUser = deleteCurrentUser;
