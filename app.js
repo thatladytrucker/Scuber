67224442a942f60ce960ff034c7dff45c2ce2b57
thatladytrucker <thatladytrucker@gmail.com>
2026-08-23 17:31:13 -0400

    Add unique ride ID generation for current ride
    Generate a unique ride ID and assign it to currentRide before creating a new ride.

--- app.js
+++ app.js
@@ -930,6 +930,17 @@ function assignAvailableDriver(){
         JSON.stringify(currentRide)
     );
 
+    const rideId =
+    "ride_" + Date.now();
+
+currentRide.id =
+    rideId;
+
+await createRide(
+    rideId,
+    currentRide
+);
+
     console.log(
         "Driver assigned:",
         driverFound
