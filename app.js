f11ce614fee9f32277b570b419d54831e1173208
thatladytrucker <thatladytrucker@gmail.com>
2026-09-11 14:35:34 -0400

    Fix My Recurring Rides navigation
    Hide multiple screens by adding 'hidden' class.

--- app.js
+++ app.js
@@ -170,6 +170,21 @@ function showMyRecurringRides(){
     document.getElementById("main-app")
     .classList.add("hidden");
 
+    document.getElementById("schedule-screen")
+    .classList.add("hidden");
+
+    document.getElementById("recurring-screen")
+    .classList.add("hidden");
+
+    document.getElementById("ride-history-screen")
+    .classList.add("hidden");
+
+    document.getElementById("ride-request-screen")
+    .classList.add("hidden");
+
+    document.getElementById("rider-trip-screen")
+    .classList.add("hidden");
+
     let recurringRides = JSON.parse(
         localStorage.getItem("scuberRecurringRides")
     ) || [];
